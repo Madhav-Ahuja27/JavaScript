ba582c94ce2fbb2e79895f725d7dbba2c57b7454
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-26 18:37:05 +0530

    Create firstJS.js

---
+++ firstJS.js
@@ -0,0 +1,35 @@
+console.log("Hello World!");
+
+alert("This is an alert");
+
+let inp = prompt("This asks for an input")
+console.log(inp, "is what you entered")
+let firstName="Madhav";
+console.log(firstName)
+
+const num=5;
+console.log(num)
+
+// undefined means there the value is literally "unknown"
+// null means the value is EMPTY
+
+let fullName = "Madhav Ahuja";
+let age = 17;
+let isMale = true;
+
+const product = {
+  name:"Ball Pen",
+  isDealDay:true,
+  price:270,
+  rating:4.5
+}
+console.log(product)
+
+const profile = {
+  name:"Madhav Ahuja",
+  followers:70,
+  age:17,
+  isFollowing:true,
+  status:"Student"
+}
+console.log(profile)
